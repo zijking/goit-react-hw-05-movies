3768e19b32e2e1881e0906d8291f0d64b9d2f30c
Fedir <zi.12feodor@gmail.com>
2020-12-25 19:47:05 +0200

    clone and fix

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import './App.css';
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
